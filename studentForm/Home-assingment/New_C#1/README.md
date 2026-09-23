6a6557f2519a7f43e44fc4efb50af0b009ce13a1
saida-blue <saidoblue62@gmail.com>
2026-09-23 22:07:44 +0100

    Add files via upload

---
+++ studentForm/Home-assingment/New_C#1/README.md
@@ -0,0 +1,2 @@
+# New_C#1
+c3 programing language
---
+++ studentForm/Home-assingment/New_C#1/studentForm/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblname_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name, department, semester;
+            name = txtname.Text;
+            department = txtdepartment.Text;
+            semester = txtsemester.Text;
+            int studentid = int.Parse(txtstudentid.Text);
+            lbloutput.Text = name + ", " + department + ", " + semester + ", " + studentid;
+
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtname.Text = "";
+            txtstudentid.Clear();
+            txtdepartment.Clear();
+            txtsemester.Text = string.Empty;
+            lbloutput.Text = string.Empty;
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
